c9804b316976856a96e5bfe40c356674c60cff38
danielepucci03 <danielepucci03@gmail.com>
2026-03-12 12:21:49 +0100

    add 2 exercise

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,6 +8,13 @@ console.log(name);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+let number = 42;
+let text = 'Hello, world!';
+let bool = true;
+
+console.log(number);
+console.log(text);
+console.log(bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
